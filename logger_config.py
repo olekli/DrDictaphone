588f3a86227f3671edb7a7f558f37fd824df1325
Ole Kliemann <mail@olekliemann.de>
2023-11-25 14:55:20 +0100

    Allow logging to file

--- logger_config.py
+++ logger_config.py
@@ -2,9 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
-logging.basicConfig(
-  level = logging.WARNING,
+config = {
+  'level': logging.WARNING,
+  'format': '%(levelname)s: %(name)s: %(message)s'
 #  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-  format='%(levelname)s: %(name)s: %(message)s'
-)
+}
+
+filename = os.environ.get('LOG_FILE', None)
+if filename:
+  config['filename'] = filename
+
+logging.basicConfig(**config)
