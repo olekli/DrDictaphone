05b7d97d4da771320552e0aeb5694e3ded61d26b
Ole Kliemann <mail@olekliemann.de>
2023-11-29 18:45:41 +0100

    Use `open` for launch script

--- package/launch.py
+++ package/launch.py
@@ -1,13 +1,6 @@
-import applescript
+import os
 import sys
 
 command = f'{sys._MEIPASS}/app/drdictaphone-internal'
 
-script = f'''
-tell application "Terminal"
-  do script "{command}"
-  activate
-end tell
-'''
-
-applescript.AppleScript(script).run()
+os.execlp('open', 'open', command)
