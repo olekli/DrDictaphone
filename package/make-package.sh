82daa4ff992960f94bda0f68a14647c2766eda59
Ole Kliemann <mail@olekliemann.de>
2023-12-15 19:14:26 +0100

    Remove kitty stuff from packaging

--- package/make-package.sh
+++ package/make-package.sh
@@ -4,7 +4,5 @@ cd package/icon &&
 sh make-icns.sh &&
 cd ../.. &&
 
-ln -fs package/drdictaphone-internal.spec . &&
-ln -fs package/DrDictaphone.spec . &&
-pyinstaller --noconfirm drdictaphone-internal.spec &&
-pyinstaller --noconfirm DrDictaphone.spec
+ln -fs package/drdictaphone.spec . &&
+pyinstaller --noconfirm drdictaphone.spec
