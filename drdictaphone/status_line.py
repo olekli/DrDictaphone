df2562e0d13c291b63273111138b2058dc63ae04
Ole Kliemann <mail@olekliemann.de>
2024-01-06 19:11:55 +0100

    PROC -> TRANS in status

--- drdictaphone/status_line.py
+++ drdictaphone/status_line.py
@@ -4,7 +4,7 @@
 def makeStatusLine(status):
   to_print = [
     'MIC' if status['mic'] else '',
-    'PROC' if status['processing'] else '',
+    'TRANS' if status['processing'] else '',
     'ERR' if status['error'] else '',
   ]
   left = '  ' + ''.join(f'{word:<6}' for word in to_print)
@@ -14,7 +14,7 @@ def makeStatusLine(status):
 def makeStatusLinePlugin(status):
   to_print = [
     'MIC' if status['mic'] else '',
-    'PROC' if status['processing'] else '',
+    'TRANS' if status['processing'] else '',
     'ERR' if status['error'] else '',
   ]
   line = f' {status["profile_name"]} ' + ''.join(f'{word:<6}' for word in to_print)
